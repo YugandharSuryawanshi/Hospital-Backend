7429960a75c0cc1ee52d92397353367cb9baf59f
Yugandhar Suryawanshi <yugandharsuryawanshi0@gmail.com>
2025-12-17 22:19:50 +0530

    Update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,7 @@ const upload = require('../middleware/upload');
 const authController = require('../controllers/authController');
 const { verifyToken } = require('../middleware/authMiddleware');
 
-// routes
+// Routes for authentication
 router.post('/register', upload.single('image'), authController.register);
 router.post('/login', authController.login);
 router.get('/me', verifyToken, authController.me);
