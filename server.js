d0d4db7bf606370e7e140440681a4048f4fd2ad6
Yugandhar Suryawanshi <yugandharsuryawanshi0@gmail.com>
2025-12-21 18:28:47 +0530

    update Main File

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const pool = require('./config/db');
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5000', credentials: true }));
 app.use(express.urlencoded({ extended: true }));
 
 // serve uploads folder
