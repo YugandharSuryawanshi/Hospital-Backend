7392e7633e546a3a7363229b6614a937744afd07
Yugandhar Suryawanshi <yugandharsuryawanshi0@gmail.com>
2026-03-06 00:32:10 +0530

    Working start on Doctor

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -15,7 +15,7 @@ exports.unreadCount = async (req, res) => {
     const userId = req.user.id;
     const [[row]] = await pool.execute(`SELECT COUNT(*) AS count FROM notifications WHERE receiver_role='doctor'
         AND receiver_id=?
-        AND is_read=FALSE`,[userId]);
+        AND is_read=FALSE`, [userId]);
     res.json({ count: row.count });
 }
 
@@ -24,3 +24,192 @@ exports.markNotificationAsRead = async (req, res) => {
     await pool.execute(`UPDATE notifications SET is_read = true WHERE receiver_role='doctor' AND receiver_id=?`, [userId]);
     res.json({ message: "Marked as read" });
 }
+
+
+
+exports.getDoctorId = async (req, res) => {
+    try {
+        const user_id = req.user.id;
+        console.log('Came User ID is :- ' + user_id);
+
+
+        const [doctor] = await pool.execute("SELECT doctor_id FROM doctors WHERE user_id = ?", [user_id]);
+        console.log('Found Doctor is :- ' + doctor);
+        console.log('Found Doctor ID is :- ' + doctor[0].doctor_id);
+
+
+        if (doctor.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "Doctor not found"
+            });
+        }
+
+        res.json({
+            success: true,
+            doctor_id: doctor[0].doctor_id
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Server Error"
+        });
+    }
+};
+
+// 1️⃣ Doctor Stats
+// =============================
+exports.getDoctorStats = async (req, res) => {
+    try {
+
+        const doctor_id = req.params.doctor_id;
+
+        console.log('Doctor ID IS Inside Stats  :- ' + doctor_id);
+
+
+        const [today] = await pool.execute(
+            `SELECT COUNT(*) as total_today
+             FROM appointments
+             WHERE doctor_id=? AND DATE(appointment_date)=CURDATE()`,
+            [doctor_id]
+        );
+
+        const [waiting] = await pool.execute(
+            `SELECT COUNT(*) as waiting
+             FROM appointments
+             WHERE doctor_id=? AND status='Pending'`,
+            [doctor_id]
+        );
+
+        const [completed] = await pool.execute(
+            `SELECT COUNT(*) as completed
+             FROM appointments
+             WHERE doctor_id=? AND status='Completed'`,
+            [doctor_id]
+        );
+
+        const [cancelled] = await pool.execute(
+            `SELECT COUNT(*) as cancelled
+             FROM appointments
+             WHERE doctor_id=? AND status='Cancelled'`,
+            [doctor_id]
+        );
+
+        const [approved] = await pool.execute(
+            `SELECT COUNT(*) as cancelled
+             FROM appointments
+             WHERE doctor_id=? AND status='Approved'`,
+            [doctor_id]
+        );
+
+        res.json({
+            total_today: today[0].total_today,
+            waiting: waiting[0].waiting,
+            completed: completed[0].completed,
+            cancelled: cancelled[0].cancelled
+        });
+
+    } catch (err) {
+        console.error("Doctor Stats Error:", err);
+        res.status(500).json({ error: err.message });
+    }
+};
+
+
+
+// =============================
+// 2️⃣ Patient Queue
+// =============================
+exports.getPatientQueue = async (req, res) => {
+
+    try {
+
+        const doctor_id = req.params.doctor_id;
+        console.log('Doctor ID IS :- ' + doctor_id);
+
+
+        const [rows] = await pool.execute(
+            `SELECT appointment_id,
+                    token_number,
+                    user_name,
+                    user_contact,
+                    appointment_time,
+                    status
+             FROM appointments
+             WHERE doctor_id=? 
+             AND DATE(appointment_date)=CURDATE()
+             ORDER BY token_number ASC`,
+            [doctor_id]
+        );
+
+        res.json(rows);
+
+    } catch (err) {
+        console.error("Queue Error:", err);
+        res.status(500).json({ error: err.message });
+    }
+};
+
+
+
+// =============================
+// 3️⃣ Today Appointments
+// =============================
+exports.getTodayAppointments = async (req, res) => {
+
+    try {
+
+        const doctor_id = req.params.doctor_id;
+
+        const [rows] = await pool.execute(
+            `SELECT appointment_id,
+                    token_number,
+                    user_name,
+                    user_contact,
+                    appointment_date,
+                    appointment_time,
+                    notes,
+                    status
+             FROM appointments
+             WHERE doctor_id=?
+             AND DATE(appointment_date)=CURDATE()
+             ORDER BY appointment_time ASC`,
+            [doctor_id]
+        );
+
+        res.json(rows);
+
+    } catch (err) {
+        console.error("Today Appointment Error:", err);
+        res.status(500).json({ error: err.message });
+    }
+};
+
+
+
+// =============================
+// 4️⃣ Update Appointment Status
+// =============================
+exports.updateAppointmentStatus = async (req, res) => {
+
+    try {
+
+        const appointment_id = req.params.appointment_id;
+        const { status } = req.body;
+
+        await pool.execute(
+            `UPDATE appointments 
+             SET status=? 
+             WHERE appointment_id=?`,
+            [status, appointment_id]
+        );
+
+        res.json({ message: "Appointment status updated successfully" });
+
+    } catch (err) {
+        console.error("Update Status Error:", err);
+        res.status(500).json({ error: err.message });
+    }
+};
--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -7,6 +7,11 @@ const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 
 router.get("/dashboard", verifyToken, isDoctor, doctorController.getDashboard);
+router.get('/getDoctorID/:id', verifyToken, isDoctor, doctorController.getDoctorId);
+router.get("/doctor/stats/:doctor_id", verifyToken, isDoctor, doctorController.getDoctorStats);
+router.get("/doctor/queue/:doctor_id", verifyToken, isDoctor, doctorController.getPatientQueue);
+router.get("//doctor/todayAppointments/:doctor_id", verifyToken, isDoctor, doctorController.getTodayAppointments);
+router.put("/doctor/updateStatus/:appointment_id", doctorController.updateAppointmentStatus);
 router.get('/notifications', verifyToken, isDoctor, doctorController.getNotifications);
 router.get('/notifications/unread-count', verifyToken, isDoctor, doctorController.unreadCount);
 router.put('/notifications/mark-read', verifyToken, isDoctor, doctorController.markNotificationAsRead);
